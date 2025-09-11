23083d4ea03a2dda83ec6efbf916d97fbfa89b55
Pythagorides <68446654+Pythagorides@users.noreply.github.com>
2025-09-11 17:18:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const data = [
     id: "event1",
     title: "Dialogue with the Deans",
     link: "https://drive.google.com/file/d/1UblTY-HLhaF3H37z6tNMLekvdmFYfdXT/view",
-    thumbnail: "https://drive.google.com/uc?export=view&id=1fkBWPC7HgsUZ1mP-VDQ49ORZldzyA4nz"
+    thumbnail: "https://drive.google.com/thumbnail?id=1fkBWPC7HgsUZ1mP-VDQ49ORZldzyA4nz&sz=w1000"
   },
   {
     id: "event2",
     title: "U.S. Law Enforcement Conversations",
     link: "https://drive.google.com/file/d/19Wsl3xnyvJfR3iiDayRbtYtBhKhvJcZc/view",
-    thumbnail: "https://drive.google.com/uc?export=view&id=1cS3g-JnmclVOeWVNfz2od1HET6NSpFGa"
+    thumbnail: "https://drive.google.com/thumbnail?id=1cS3g-JnmclVOeWVNfz2od1HET6NSpFGa&sz=w1000"
   },
   {
     id: "event3",
     title: "U.S. Supreme Court Conversations",
     link: "https://drive.google.com/file/d/1FMQnUd5nG86Ybm3gL1zdB7fkjO12isHr/view",
-    thumbnail: "https://drive.google.com/uc?export=view&id=1pJQXj5F2mSffewnM8rKPeMd07PNRwH3x"
+    thumbnail: "https://drive.google.com/thumbnail?id=1pJQXj5F2mSffewnM8rKPeMd07PNRwH3x&sz=w1000"
   }
 ];
 
