14b01deb1257964ee96d765373eaa796398f996d
Pythagorides <68446654+Pythagorides@users.noreply.github.com>
2025-09-12 10:42:56 -0400

    Update entries.js Debug

--- entries.js
+++ entries.js
@@ -1,27 +1,7 @@
-/*
-==========================
- NEW ENTRY QUICK TEMPLATE
-==========================
-1. Upload the PDF to Google Drive → set to "Anyone with the link can view".
-2. Copy the link and extract the FILE_ID (between /d/ and /view).
-3. Upload the thumbnail image to your Google Drive "Images" folder → set to "Anyone with the link can view".
-4. Copy the link and extract the FILE_ID.
-5. Paste into the object below, replacing PASTE_PDF_FILE_ID_HERE and PASTE_IMAGE_FILE_ID_HERE.
-6. Add the object to the data array, making sure there’s a comma after the previous entry.
-
-Example:
-{
-  id: "eventX", // Change X to the next number in sequence
-  title: "Your Entry Title Here",
-  link: "https://drive.google.com/file/d/PASTE_PDF_FILE_ID_HERE/view",
-  thumbnail: "https://drive.google.com/thumbnail?id=PASTE_IMAGE_FILE_ID_HERE&sz=w1000"
-}
-*/
-
 const data = [
   {
     id: "event1",
-    title: "Vajrakilaya or Mikey and the Liver Pirates རྡོ་རྗེ་ཕུར་པ། ",
+    title: "Vajrakilaya or Mikey and the Liver Pirates རྡོ་རྗེ་ཕུར་པ།",
     link: "https://drive.google.com/file/d/16J9uzTQDYw_oA-TI9KBtSPQDXgvl2y_u/view",
     thumbnail: "https://drive.google.com/thumbnail?id=1PH1_8Il0rXkGXtqYknD77OAY72k2XRMB&sz=w1000"
   },
@@ -33,86 +13,83 @@ const data = [
   },
   {
     id: "event3",
-    title: "A Liitle Madness Goes a Long Way",
+    title: "A Little Madness Goes a Long Way",
     link: "https://drive.google.com/file/d/1wsROPXKMz1PR1d0bMc8UV6k-IBF1IB_q/view",
     thumbnail: "https://drive.google.com/thumbnail?id=1Jd4m06WphjZyga2xhTegmcr8k7plwF4y&sz=w1000"
-  }
- {
-    id: "event4”,
+  },
+  {
+    id: "event4",
     title: "Vajrasattva or A Crypto-Straussian Lament རྡོ་རྗེ་སེམས་དཔའ།",
     link: "https://drive.google.com/file/d/15mpvoEJnqZD3bRC69A8cOL5SHIJnldPj/view",
     thumbnail: "https://drive.google.com/thumbnail?id=1Fo9vtoSFapX_j_gBGdDetV-lMnbT0u7S&sz=w1000"
   },
- {
-    id: "event5”,
+  {
+    id: "event5",
     title: "Midnight",
     link: "https://drive.google.com/file/d/165sLV_Jhn9h_qfRyTl0BtcpNeib4eJ8t/view",
     thumbnail: "https://drive.google.com/thumbnail?id=1AIi1BLuJfAjJdia-819_4kw50fLoD0Op&sz=w1000"
   },
- {
-    id: "event6”,
-    title: "Yamantaka or The Mask of Sisera  གཤིན་རྗེ་གཤེད་",
+  {
+    id: "event6",
+    title: "Yamantaka or The Mask of Sisera གཤིན་རྗེ་གཤེད་",
     link: "https://drive.google.com/file/d/1xHnGi127fRjlVlqcroCDJrFvLmIOenK3/view",
     thumbnail: "https://drive.google.com/thumbnail?id=1NzM5MX9pIP9_huhMPSqJs7yAHA-jWsUw&sz=w1000"
   },
- {
-    id: "event7”,
+  {
+    id: "event7",
     title: "Gemelli",
     link: "https://drive.google.com/file/d/1623xvcSHa9Agxtr7RFeMI-SDuLLWv8NQ/view",
     thumbnail: "https://drive.google.com/thumbnail?id=1Ggd7GvPRY_9YrSDUITefI3Lp9gMF8sE_&sz=w1000"
   },
- {
-    id: "event8”,
+  {
+    id: "event8",
     title: "Avelokitesvara or Jacob's Ladder སྤྱན་རས་གཟིགས་",
     link: "https://drive.google.com/file/d/1xB6MaekErdWB_SUs7lB8qtcDuXepTS8R/view",
     thumbnail: "https://drive.google.com/thumbnail?id=1VoAnnmJrfgHEwUeukoQ-F0faMGEaewd0&sz=w1000"
   },
- {
-    id: "event9”,
+  {
+    id: "event9",
     title: "Mahakala ནག་པོ་ཆེན་པོ།",
     link: "https://drive.google.com/file/d/163u4ovQ6e2FgbAlm1ODiYC2-CJldRDDh/view",
     thumbnail: "https://drive.google.com/thumbnail?id=1zYIghPMQPKyLGLUhegCLeEHGe4ZxRH90&sz=w1000"
   },
- {
-    id: "event10”,
+  {
+    id: "event10",
     title: "In the Hands of the Goddess",
     link: "https://drive.google.com/file/d/1wBVt9R-1Gh-c8WDovkoktPD_hEOkcdxp/view",
     thumbnail: "https://drive.google.com/thumbnail?id=1VjmJfrmhQaWJoZm43iIgDWDErA89l63u&sz=w1000"
   },
- {
-    id: "event11”,
+  {
+    id: "event11",
     title: "Ksitigarbha or The Philosopher's Daughters ས་ཡི་སྙིང་པོ་",
     link: "https://drive.google.com/file/d/15ih8ugq_LE-D4dQ1KGub2xdzGUQulIUM/view",
     thumbnail: "https://drive.google.com/thumbnail?id=16U0-G35qGjnYhWE_fg4rLlhl43c943MX&sz=w1000"
   },
- {
-    id: "event12”,
+  {
+    id: "event12",
     title: "Namju Wangden or The Great Vow དུས་ཀྱི་འཁོར་ལོ།",
     link: "https://drive.google.com/file/d/1xPujJGVxrAN80VYoP09z_7H2RmzuwwwG/view",
     thumbnail: "https://drive.google.com/thumbnail?id=1xS0G_OkksIScgAIFS7qmQUS76CoBxsMI&sz=w1000"
   },
- {
-    id: "event13”,
-    title: "Ksitigarbha or Amor Vincit Omnia ས་ཡི་སྙིང་པོ་ ",
+  {
+    id: "event13",
+    title: "Ksitigarbha or Amor Vincit Omnia ས་ཡི་སྙིང་པོ་",
     link: "https://drive.google.com/file/d/1nrbqFA1eQ13JPJthDVumXO495BJ9KBhd/view",
     thumbnail: "https://drive.google.com/thumbnail?id=1Qu3Ncz-zGLJutbPqt9Q9j7IrQuaLKmRV&sz=w1000"
   },
- {
-    id: "event14”,
+  {
+    id: "event14",
     title: "Ucchusma སྨེ་བརྩེགས་",
     link: "https://drive.google.com/file/d/1x-qPd20Dvl5_7Jhj0gHhnB9fbHpcWUHZ/view",
     thumbnail: "https://drive.google.com/thumbnail?id=12JFdRoPah1xB33UaI21Y2EsUVDoe0FNc&sz=w1000"
   },
- {
-    id: "event15”,
+  {
+    id: "event15",
     title: "H.H. Dorje Chang Buddha III རྡོ་རྗེ་འཆང།",
     link: "https://drive.google.com/file/d/16DOdNW-Sz7SKCa_iJc2ILtmgN1dHnn8F/view",
     thumbnail: "https://drive.google.com/thumbnail?id=15jcmpCEAA8A8icWMoJxpsrRWeGPL6n8Q&sz=w1000"
   },
- {
-    id: "event16”,
+  {
+    id: "event16",
     title: "Tsanbala or The Yellow God of Wealth རྣམ་ཐོས་སྲས",
-    link: "https://drive.google.com/file/d/1mWxKETjk7QpSDZIl-L59F6Xotlg0AmMz/view",
-    thumbnail: "https://drive.google.com/thumbnail?id=1jgcVK6y0QKVlJkJW8kwfKr-U7Z1eGbdp&sz=w1000"
-  },
-];
+    link: "https://drive
