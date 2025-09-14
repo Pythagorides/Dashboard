2e061ada2e45e3cc313886927bf83090dd33c689
Michael Ramon Ochoa <michaelochoa@mac.com>
2025-09-14 17:09:24 -0400

    Refactored shared rendering logic and added Deductive Science entries

---
+++ js/deductive-entries.js
@@ -0,0 +1,111 @@
+const entries = [
+  {
+    id: "event1",
+    title: "1991-1995 Authorities",
+    link: "https://drive.google.com/file/d/1aS_Ms8J8PW4l5nKVvnlI7ORypR7O9JAJ/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1_aUVfcOehhFKTeXLspFvjtTFvEBC9496&sz=w1000"
+  },
+  {
+    id: "event2",
+    title: "1993 Preliminary Prospectus",
+    link: "https://drive.google.com/file/d/1tiurAAVBINwbRfcQy-USIwPS1dKGMukE/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1CHQ84KSMH8W7D1BdO3_YMPz4gAOqkuBe&sz=w1000"
+  },
+  {
+    id: "event3",
+    title: "1993-05-00 Frege and Russell on the Semantics of Definite Referring Expressions",
+    link: "https://drive.google.com/file/d/1t9on6lUbfFOgpjD5zktM9-myE-lVba67/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1OxiScFp5GVudukqpT2dW4XvOiAbacNQ-&sz=w1000"
+  },
+  {
+    id: "event4",
+    title: "1993-1994 Liar Paradox 1",
+    link: "https://drive.google.com/file/d/1t9on6lUbfFOgpjD5zktM9-myE-lVba67/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1hhPAVH_8P_72_cWbcoyB4HNBe_M7KqH-&sz=w1000"
+  },
+  {
+    id: "event5",
+    title: "1993-1994 Liar Paradox 2",
+    link: "https://drive.google.com/file/d/1taulNL8goV3eBzMh3SyKvERMt6Q9kmjT/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=12Cv-3z_tzpomQZpqgFS50AVrkKTu43Di&sz=w1000"
+  },
+  {
+    id: "event6",
+    title: "1993-1994 Liar Paradox 3",
+    link: "https://drive.google.com/file/1tTbuWaQINcE5A-TGvsBAgZ620w0d8ndQ/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1nuqG353PLKNysd7sKpIiG-YpjavrvbQC&sz=w1000"
+  },
+  {
+    id: "event7",
+    title: "1993-1994 Liar Paradox 4",
+    link: "https://drive.google.com/file/d/1thTDUW4ISsKlqyzie9Xe6elIl32Vdv_m/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=19XFTNLBChKklpaF1u2EjFnuFxb8I23WT&sz=w1000"
+  },
+  {
+    id: "event8",
+    title: "1994 Liar Abstract",
+    link: "https://drive.google.com/file/d/1thTDUW4ISsKlqyzie9Xe6elIl32Vdv_m/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1FWMe-AISqWjA9Jbdvh72o7qXqCmgtMe8&sz=w1000"
+  },
+  {
+    id: "event9",
+    title: "1994 Paradox Notebook Tigers",
+    link: "https://drive.google.com/file/d/1tm8R01qFOIvfIz2YyPBdwgR34X6dPURV/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1xrhBzoms-uVpliKKXS_uRF_--btetC-K&sz=w1000"
+  },
+  {
+    id: "event10",
+    title: "1994-10-14 A Philosophical and Mathematical Theory of Truth 1",
+    link: "https://drive.google.com/file/d/1tBwRLD0mtPDa0O20pTY-DvZNRna58VU_/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1CCQcbVSXYIBry7wn29EViCc9228fbs7l&sz=w1000"
+  },
+  {
+    id: "event11",
+    title: "1994-10-14 A Philosophical and Mathematical Theory of Truth 2",
+    link: "https://drive.google.com/file/d/1tio95buhpgcmlehccB1wFjkHOaebCXhK/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=13EoHty3guVa4LF9l3gEUZL9RsVqncCF6&sz=w1000"
+  },
+  {
+    id: "event12",
+    title: "1994-11-07 A Philosophical and Mathematical Theory of Truth 3",
+    link: "https://drive.google.com/file/d/1tezKIZLoDKn8Z72WUiSc3qTd_g40HqRF/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1aSOO2aczsGBQO4nwAbxmtVCC-DfqNA0h&sz=w1000"
+  },
+  {
+    id: "event13",
+    title: "1995 Paradox Notebook.Green",
+    link: "https://drive.google.com/file/d/1u6p5m-sEaFdjGLyQSXEspREcgEImtHJv/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1zaV7Lp1ZEYLdw5SU2uLplOT9e4fbqpOh&sz=w1000"
+  },
+  {
+    id: "event14",
+    title: "1995-05-00 NEH Dissertation Grants Abstract of Dissertation Project་",
+    link: "https://drive.google.com/file/d/1tbRe9eyeBp6XalYlAcGpJ3J3HnJ5TJ-h/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1gGMfbAVIpPsfHWeF-BEfvsVx90gnb453&sz=w1000"
+  },
+  {
+    id: "event15",
+    title: "1996 Paradox Notebook Purple Five Star",
+    link: "https://drive.google.com/file/d/1ttnVGcaGg7Okv3gwAgD0pD-lVgEeW3bP/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1S9oPokuQdVYlNXBEoJE0JcXtW1F9M0kU&sz=w1000"
+  },
+  {
+    id: "event16",
+    title: "1997 Paradox Notebook Blue Suffolk",
+    link: "https://drive.google.com/file/d/1u7WRx83K4HiN_BiW9KuLi7vBIOgFb2zb/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1GjEFfFhFIvzYgORDe7vn20MpSBBEssAc&sz=w1000"
+  },
+   {
+    id: "event17",
+    title: "2001 Paradox Notebook Orange",
+    link: "https://drive.google.com/file/d/1tTksb7WcpB1eSpl7l1he5CkrNdJ-g7_v/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1rrPZwPenpXm_uMzlVOxZc7A2KROGGeQt&sz=w1000"
+  },
+   {
+    id: "event18",
+    title: "2016-11-07 A Philosophical and Mathematical Model of Truth or #NewOrganon",
+    link: "https://drive.google.com/file/d/1suhOjVIPCbmoMo4VWoJ5Ap6kAUDEXSai/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=192LP8ZkTgMMytOsZRLqk8woYRzHRbQnM&sz=w1000"
+  }
+]; 
+
---
+++ js/shared-script.js
@@ -0,0 +1,29 @@
+function renderEntries(entries) {
+  const timeline = document.getElementById('timeline');
+
+  entries.forEach(entry => {
+    const item = document.createElement('div');
+    item.className = 'timeline-item';
+
+    const title = document.createElement('h2');
+    title.textContent = entry.title;
+
+    const link = document.createElement('a');
+    link.href = entry.link;
+    link.target = "_blank";
+
+    const img = document.createElement('img');
+    img.src = entry.thumbnail;
+    img.alt = `Thumbnail of ${entry.title}`;
+
+    img.onerror = () => {
+      img.src = "https://via.placeholder.com/200x280?text=Image+Not+Found";
+      img.alt = `No thumbnail available for ${entry.title}`;
+    };
+
+    link.appendChild(img);
+    item.appendChild(title);
+    item.appendChild(link);
+    timeline.appendChild(item);
+  });
+}
