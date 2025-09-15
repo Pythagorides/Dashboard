8093f81b7c16be55d21789a8fbd3ce4f0a7e236a
Michael Ramon Ochoa <michaelochoa@mac.com>
2025-09-15 13:25:40 -0400

    Refactored shared rendering logic and added Deductive Science entries

---
+++ js/inductive-entries.js
@@ -0,0 +1,57 @@
+const entries = [
+  {
+    id: "event1",
+    title: "1995-08-01 Morphometric and Immunohistochemical Characterization of Human Liver Regeneration།",
+    link: "https://drive.google.com/file/d/1Yy3aOXuY-zB7Z8Edi9ZPNRzqBukYP-GA/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1L-3sG16K_K3jW7NkCJkLShE28GIGNuMR&sz=w1000"
+  },
+  {
+    id: "event2",
+    title: "2000-01-18 A First Step Toward Understanding the Molecular Basis of Engineered In Vitro Hepatogenesis",
+    link: "https://drive.google.com/file/d/1ZpfO6DmR-N25r53kOBrc2grpU3bXV8gV/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1kNvJb3T7jxzpRk7M3Ukdgcl3pNfBiyEF&sz=w1000"
+  },
+  {
+    id: "event3",
+    title: "2001-10-01 Developing a Core Platform for the Tissue Engineering of Vital Organs",
+    link: "https://drive.google.com/file/d/1gW07vYdFkglktdKrB1o7Q-hLe8_81Rr5/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1ORZlvdB3zUSW7kLty_AU6i9xlpM3W298&sz=w1000"
+  },
+  {
+    id: "event4",
+    title: "2002-08-01 An Overview of the Pathology and Approaches to Tissue Engineering།",
+    link: "https://drive.google.com/file/d/1dBK_1an8z_TmaztRTGlLuiCUbTQ88uwx/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1GaLhJ0A73M-NeD8HANo6_q1c38eYjknZ&sz=w1000"
+  },
+  {
+    id: "event5",
+    title: "2003-01-22 NIDDK K08 Targeting p27kip1 to Improve Hepatocyte Transplantation",
+    link: "https://drive.google.com/file/d/1f0iNrv_Xm7IAuoMQ2y43ai8h8dmNgbgG/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1Nm8OZo52S9ahcPKMDmr0RMxKNJHZKG3V&sz=w1000"
+  },
+  {
+    id: "event6",
+    title: "2006-06-08 Hepatocyte Nuclear Factor Data Set U Pitt IRB 0501051་",
+    link: "https://drive.google.com/file/d/1elEZRCGyUNOwddFVbf36Zhq_EoqEL4hF/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1GJFmxUBcUqOlwbJuVo3smwDI2B6Lix0-&sz=w1000"
+  },
+  {
+    id: "event7",
+    title: "2006-07-30 Toward a Computable Model of Epithelial Differentiation in Hepatogenesis and Liver Regeneration",
+    link: "https://drive.google.com/file/d/1ejr1BG9MCyCOgHFTlDnV2upwNksUuXwe/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1koMih6rys_WD30Guw43Z270r9ZHC6koA&sz=w1000"
+  },
+  {
+    id: "event8",
+    title: "2006-11-07 Liver Doodles",
+    link: "https://drive.google.com/file/d/1mFHaCZgMZxco5f_6vD8Z_vPzqGCekzAP/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1ACx5CGyor2JWtpOPZJBlerqhWiW0uEWC&sz=w1000"
+  },
+  {
+    id: "event9",
+    title: "2009-02-03 Laptop Documents Clone to SF FBI",
+    link: "https://drive.google.com/file/d/1axlORVAEx8TcGgJCrCgoNlf9vh1dm5HW/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=15MWLIdl_SYZJGquiZIHNvFOhKERfjNi7&sz=w1000"
+  },
+]; 
+
---
+++ js/inductive-script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  renderEntries(entries);
+});
+
+
