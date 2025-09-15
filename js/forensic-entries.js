b402855021817b9890dcf67b7524c3d9d2389ceb
Michael Ramon Ochoa <michaelochoa@mac.com>
2025-09-15 15:11:30 -0400

    Added Forensic Science page and entries

---
+++ js/forensic-entries.js
@@ -0,0 +1,75 @@
+const entries = [
+  {
+    id: "event1",
+    title: "Forensic Science or #UPMCRecycles",
+    link: "https://drive.google.com/file/d/1W7P3lZTCCA91-O3LlxICd5x9ZK_ZZ-81/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1lrpsOxO0jEkk9qO6D4p0cRcaiBWaDEyo&sz=w1000"
+  },
+  {
+    id: "event2",
+    title: "Exhibits Dashboard",
+    link: "https://drive.google.com/file/d/17spIyJv3Szy5uC-qZwR0ZX4diOupds6L/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1o4ShQDeZjLNFqvCMb_YazcNXB1wbrlGS&sz=w1000"
+  },
+  {
+    id: "event3",
+    title: "Root Facts",
+    link: "https://drive.google.com/file/d/18KXrw4W50Cvwt5x0_nkdYDEnsQMXnzMh/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=16SexYrv3q5E_S2rYPItf5gN6anhqHXNx&sz=w1000"
+  },
+  {
+    id: "event4",
+    title: "Event Matrix",
+    link: "https://drive.google.com/file/d/1Sx2cgCuixB2TXI01gpXdbljdFkFGI7vB/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1idkagoIapeAsVFlE6Y4wAslrzsWJwcSY&sz=w1000"
+  },
+  {
+    id: "event5",
+    title: "UPMC Medical Records Dashboard",
+    link: "https://drive.google.com/file/d/1r8-OCIVT_41Rmpcl11rdEg-hTfQ12uUi/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1z_t_cPun548mzbN4zq38KXnTqhRbPMo5&sz=w1000"
+  },
+  {
+    id: "event6",
+    title: "Defendants",
+    link: "https://drive.google.com/file/17p1dtOYsJaIZo8hCUlhjoZ0EemstPZVr/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1ZTn8ODAiAcmHtbJY6br2D9k3OLnfhTTr&sz=w1000"
+  },
+  {
+    id: "event7",
+    title: "Dialogue with the Deans",
+    link: "https://drive.google.com/file/d/1UblTY-HLhaF3H37z6tNMLekvdmFYfdXT/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1KiJWKxAfJ1c7n0-SURrRmmP0Mem79pR5&sz=w1000"
+  },
+  {
+    id: "event8",
+    title: "Conversations with the Late, Honorable Ralph J. Cappy",
+    link: "https://drive.google.com/file/d/17e5_URswTuvRmoDBjr5w0pdUJFWDpap_/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1tRz5EybSQNWE4IjGn9pVnvCh12R2EV5l&sz=w1000"
+  },
+  {
+    id: "event9",
+    title: "U.S. Law Enforcement Conversations",
+    link: "https://drive.google.com/file/d/19Wsl3xnyvJfR3iiDayRbtYtBhKhvJcZc/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1cS3g-JnmclVOeWVNfz2od1HET6NSpFGa&sz=w1000"
+  },
+  {
+    id: "event10",
+    title: "U.S. Supreme Court Conversations",
+    link: "https://drive.google.com/file/d/1FMQnUd5nG86Ybm3gL1zdB7fkjO12isHr/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1pJQXj5F2mSffewnM8rKPeMd07PNRwH3x&sz=w1000"
+  },
+  {
+    id: "event11",
+    title: "Items of Judicial Notice",
+    link: "https://drive.google.com/file/d/1HZhIidj36DR8qniJvSS4cO4_85_m1YBp/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=164azbfIKOE1GLn9lYmsWEVW3PkHDhddm&sz=w1000"
+  },
+  {
+    id: "event12",
+    title: "Questions or Aporias",
+    link: "https://drive.google.com/file/d/18JcdCdZUf0IPXuUXDQ6chuOC4aKVVFcG/view",
+    thumbnail: "https://drive.google.com/thumbnail?id=1U4DE_c0fuCrsYYwuZAkGUi6u3xVTKanF&sz=w1000"
+  },
+]; 
+
---
+++ js/forensic-script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+  renderEntries(entries);
+});
